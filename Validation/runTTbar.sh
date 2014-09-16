fdce053a6067a540cfad30b5298e20593ee0c332
Matthias <mkomm@cern.ch>
2014-09-16 17:46:18 +0200

    refine val script

--- Validation/runTTbar.sh
+++ Validation/runTTbar.sh
@@ -3,24 +3,27 @@ FOLDER=TTbarValidation_$1
 mkdir $FOLDER
 cd $FOLDER
 
-cmsDriver.py TTbar_8TeV_cfi.py \
--s GEN,SIM,RECO,HLT,VALIDATION \
---fast \
---pileup=NoPileUp \
---eventcontent FEVTDEBUGHLT,DQM \
---datatier GEN-SIM-DIGI-RECO,DQMIO \
---conditions auto:startup_GRun \
---beamspot Realistic8TeVCollision \
--n 1000 \
---fileout=file:TTbarValidation_$1.root \
---python_filename=TTbarValidation_$1_cfg.py \
---mc
+NSIZE=$2
 
-cmsDriver.py step3 \
+if [[ -z "$NSIZE" ]]; then NSIZE=10; fi
+echo $NSIZE
+
+cmsDriver.py TTbar_Tauola_8TeV_cfi  \
+--conditions auto:run1_mc \
+--fast  \
+-n $NSIZE \
+-s GEN,SIM,RECO,EI,HLT:@relval,VALIDATION \
+--eventcontent DQM \
+--relval None \
+--datatier DQMIO \
+--fileout file:step2.root
+
+cmsDriver.py step3  \
+--scenario pp \
+--filetype DQM \
+--conditions auto:run1_mc \
+--mc  \
 -s HARVESTING:validationHarvestingFS \
---harvesting AtRunEnd \
---conditions auto:startup \
---mc \
---filein file:TTbarValidation_$1.root \
---fileout file:TTbarValidation_DQMPlots_$1.root \
---python_filename=TTbarValidation_DQMPlots_$1_cfg.py 
+-n $NSIZE  \
+--filein file:step2.root \
+--fileout file:step3.root 
